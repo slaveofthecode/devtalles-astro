7db39b10ef9728837728f388de69eff02c02cbd1
slaveofthecode <gml.gustavoml@gmail.com>
2024-08-26 23:28:31 -0300

    Fetch data from Pokemon API

---
+++ src/types/pokemon-list.response.ts
@@ -0,0 +1,18 @@
+export interface Root {
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
+}
+
+export interface Result {
+  name: string;
+  url: string;
+}
+
+export interface PokemonListResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
+}
